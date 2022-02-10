1f54f918009021edc6f578d6c33e8bc483abdb88
Z3R1NH0 <98356076+Z3R1NH0@users.noreply.github.com>
2022-02-10 19:01:36 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,17 +3,24 @@ var x;
 
 function setup() {
   createCanvas(400,400);
+ 
+  teste = createSprite (200,200,30,15);
+
+  //teste.velocityX = 3;
+
+ 
 }
 
 function draw() 
 {
-  background("black");
+   background("black");
 
-  teste = createSprite (200,200,30,15);
+  if (keyDown ("s")){
+ teste.y = teste.y + 4;
+  }
   
   drawSprites();
 }
-
 
 
 
