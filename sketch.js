994cd2bf17b5c67c8c6f1ab7f94b30ef3989e1ce
Z3R1NH0 <98356076+Z3R1NH0@users.noreply.github.com>
2022-02-10 18:46:14 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var teste;
+var x;
+
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+  background("black");
+
+  teste = createSprite (200,200,30,15);
+  
+  drawSprites();
+}
+
+
+
+
